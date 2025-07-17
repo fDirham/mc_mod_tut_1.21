887a8b3a7b5223ebd570f8f7f832a6e206f82fe9
FBDPC <fajarletters@gmail.com>
2025-07-16 21:25:15 -0700

    Custom item

--- src/main/java/com/fbdco/tutorialmod/TutorialMod.java
+++ src/main/java/com/fbdco/tutorialmod/TutorialMod.java
@@ -1,7 +1,9 @@
 package com.fbdco.tutorialmod;
 
+import com.fbdco.tutorialmod.item.ModItems;
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -35,6 +37,9 @@ public class TutorialMod
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
+
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -50,6 +55,10 @@ public class TutorialMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.ALEXANDRITE);
+            event.accept(ModItems.RAW_ALEXANDRITE);
+        }
 
     }
 
